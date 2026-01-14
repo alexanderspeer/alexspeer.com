82d57ca6e0a43674b55dcf54f7cac0135d61208f
alexanderspeer <alexspeer12@gmail.com>
2026-01-14 03:14:44 -0500

    added a few new projects

--- src/pages/projects/main.js
+++ src/pages/projects/main.js
@@ -165,7 +165,7 @@ function getGalleryImages(folder) {
         'euterpe': [1, 2, 3, 4, 5, 6, 7, 8, 9],
         'facemash': [1, 2, 3],
         'bookshelf': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28],
-        'blindsight': [1, 2, 3, 4, 5, 6]
+        'blindsight': [1, 2, 3, 4, 5, 6, 7, 8, 9]
     };
 
     const imageNumbers = galleryImages[folder] || [];
